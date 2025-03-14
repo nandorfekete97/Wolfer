eed7a28464d94babc20dde462b2315f3faad475b
Fekete Nándor <nandor.fekete@yahoo.com>
2025-03-14 13:10:06 +0100

    initial db migration created (primary keys specified for entities); db config placed in Program.cs

--- Wolfer/Data/Entities/TrainingEntity.cs
+++ Wolfer/Data/Entities/TrainingEntity.cs
@@ -1,5 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
 namespace Wolfer.Data.Entities;
 
+[PrimaryKey(nameof(Id))]
 public class TrainingEntity
 {
     public int Id;
--- Wolfer/Program.cs
+++ Wolfer/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Wolfer.Data.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<WolferContext>(options =>
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:WolferDb"]));
 
 var app = builder.Build();
 
