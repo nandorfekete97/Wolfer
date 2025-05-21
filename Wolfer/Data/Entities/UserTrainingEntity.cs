1e1f746df796bc3a3c4e82e65c335a87102dc2f0
Fekete Nándor <nandor.fekete@yahoo.com>
2025-05-21 15:35:44 +0200

    removed space;

--- Wolfer/Data/Entities/UserTrainingEntity.cs
+++ Wolfer/Data/Entities/UserTrainingEntity.cs
@@ -6,7 +6,6 @@ namespace Wolfer.Data.Entities;
 [PrimaryKey(nameof(UserId), nameof(TrainingId))]
 public class UserTrainingEntity
 {
-    
     public Guid UserId { get; set; }
     public int TrainingId { get; set; }
 }
