70336d8b7b0d5f7d3a5a73a1471b07d1d98b24d2
Fekete Nándor <nandor.fekete@yahoo.com>
2025-05-23 14:46:46 +0200

    logout button refining

--- wolfer_frontend/src/Components/Layout/Layout.jsx
+++ wolfer_frontend/src/Components/Layout/Layout.jsx
@@ -10,10 +10,18 @@ const Layout = ({setSuccessfulLogin}) => {
 
   return (
       <div className="align-items-start">
-          <Sidebar />
-          <button onClick={(e) => handleLogout(e)}>
-            Logout
-          </button>
+          {/* <Sidebar /> */}
+          <ul className="list-unstyled">
+            <li className="sidebar-item">Trainings</li>
+            <li className="sidebar-item">Profile</li>
+            <li className="sidebar-item">PRs</li>
+            <li className="sidebar-item">Training History</li>
+            <li>
+              <button className="sidebar-item" id="logout-button" onClick={(e) => handleLogout(e)}>
+                Logout
+              </button>
+            </li>
+          </ul>
       </div>
   );
 };
