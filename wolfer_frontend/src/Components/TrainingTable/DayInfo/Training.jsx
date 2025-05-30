2b3732f92f3bc4d8b8d11e72dc5335d779206ceb
Fekete Nándor <nandor.fekete@yahoo.com>
2025-05-30 08:54:37 +0200

    console.logs for debug purposes

--- wolfer_frontend/src/Components/TrainingTable/DayInfo/Training.jsx
+++ wolfer_frontend/src/Components/TrainingTable/DayInfo/Training.jsx
@@ -36,6 +36,9 @@ const Training = ({ training, signedUpTrainingIdsForDay, refreshSignedUpTraining
     e.preventDefault();
     const userId = localStorage.getItem("userId");
 
+    console.log("today: ", today.toISOString());
+    console.log("training.date: ", training.date);
+
     try {
         const response = await fetch(`${import.meta.env.VITE_API_URL}/UserTraining/SignUserUpForTraining/users/${userId}/trainings/${training.id}`, {
             method: 'POST',
