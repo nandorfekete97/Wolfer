d6b6525472724d7c698f0ce07db868f0d762d600
Fekete Nándor <nandor.fekete@yahoo.com>
2025-06-12 17:26:34 +0200

    past trainings are successfully transferred to frontend

--- wolfer_frontend/src/Components/TrainingHistory/TrainingHistory.jsx
+++ wolfer_frontend/src/Components/TrainingHistory/TrainingHistory.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 
 const TrainingHistory = () => {
+
+const [pastTrainings, setPastTrainings] = useState([]);
+const [dataIsLoaded, setDataIsLoaded] = useState(false);
+
+const getpastTrainings = async () => {
+    const userId = localStorage.getItem("userId");
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/UserTraining/GetPastTrainingsForUser/${userId}`);
+    const data = await res.json();
+    setPastTrainings(data.trainingEntities); 
+    console.log("user id: ", userId);
+    console.log("pastTrainings: ", pastTrainings);
+    setDataIsLoaded(true);
+}
+
+useEffect(() => {
+    getpastTrainings();
+}, []);
+
+useEffect(() => {
+    console.log("Updated pastTrainings:", pastTrainings);
+}, [pastTrainings]);
+
+
   return (
     <div>
-      TRAINING HISTORY
+      <h1>TRAINING HISTORY</h1>
+      {dataIsLoaded ? 
+      <ul>
+        <li> {pastTrainings.map(training => training.date)} </li>
+      </ul>
+      : 
+      "Loading"}
     </div>
   )
 }
 
-export default TrainingHistory
+export default TrainingHistory;
