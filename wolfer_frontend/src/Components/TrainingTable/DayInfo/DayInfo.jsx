8b043802303c623ff297afe4722b05b08f4e1e43
nandorfekete <nandorfekete@Nandors-MacBook-Pro.local>
2025-07-29 14:39:52 +0200

    delete trainings for day fully functional;

--- wolfer_frontend/src/Components/TrainingTable/DayInfo/DayInfo.jsx
+++ wolfer_frontend/src/Components/TrainingTable/DayInfo/DayInfo.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useImperativeHandle, useState } from 'react';
 import Training from './Training'
 import './DayInfo.css'
+import DeleteTrainingsByDateModal from '../../Modals/DeleteTrainingsByDateModal';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const DayInfo = ({ date, signedUpTrainings, refreshSignedUpTrainings, showSignUp = true, refreshTrigger, triggerRefresh, isSelectedDateToday }) => {
   
   const [trainings, setTrainings] = useState([]);
   const [signedUpTrainingIdsForDay, setSignedUpTrainingIdsForDay] = useState([]);
+  const [deleteTrainingsByDateModalIsOpen, setDeleteTrainingsByDateModalIsOpen] = useState(false);
 
   const getTrainings = async () => {
     const dateOnly = date.toLocaleDateString('sv-SE');
@@ -47,9 +51,46 @@ const DayInfo = ({ date, signedUpTrainings, refreshSignedUpTrainings, showSignUp
     }
   }, [date, refreshTrigger]);
 
+  const handleDeleteAll = async (e) => {
+  e.preventDefault();
+
+  const dateOnly = date.toISOString().split("T")[0];
+
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/Training/DeleteTrainingsByDate/${dateOnly}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+
+      if (response.ok) {
+        toast.success("Trainings were deleted successfully.");
+        setDeleteTrainingsByDateModalIsOpen(false);
+        getTrainings();
+      } else {
+        const data = await response.json();
+        toast.error(data.message || 'Trainings could not be deleted.');
+      }
+    } catch (error) {
+      toast.error('An error occurred during deleting trainings.');
+    }
+  }
+
   return (
     <>
-        <h3 className="day-info"> {date ? `${date.toDateString()}` : ""} </h3>
+        <h3 className="day-info">
+          <div>
+            {date ? `${date.toDateString()}` : ""} 
+            <button
+              className = "btn btn-sm btn-danger delete-button"
+              // disabled = {isPast}
+              onClick = {(e) => setDeleteTrainingsByDateModalIsOpen(true)}
+            >
+              Delete All
+            </button>
+          </div> 
+        </h3>
         {trainings.map((training) => (
           <h5 key = {training.id}>
             <Training
@@ -64,6 +105,13 @@ const DayInfo = ({ date, signedUpTrainings, refreshSignedUpTrainings, showSignUp
             />
           </h5>
         ))}
+
+        <DeleteTrainingsByDateModal
+          deleteTrainingsByDateModalIsOpen = {deleteTrainingsByDateModalIsOpen}
+          closeDeleteModal = {() => setDeleteTrainingsByDateModalIsOpen(false)}
+          handleDeleteAll = {handleDeleteAll}
+          date = {date}
+        />
     </>
   )
 }
