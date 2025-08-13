58edc9cdfc57b185dd5604cdc4511bce8b2c7518
nandorfekete <nandorfekete@Nandors-MacBook-Pro.local>
2025-08-13 14:56:29 +0200

    ProfilePhoto successfully created in db;

--- Wolfer/Data/Context/WolferContext.cs
+++ Wolfer/Data/Context/WolferContext.cs
@@ -11,8 +11,25 @@ public class WolferContext : IdentityDbContext<IdentityUser, IdentityRole, strin
     public DbSet<TrainingEntity> Trainings { get; set; }
     public DbSet<TrainerEntity> Trainers { get; set; }
     public DbSet<PersonalRecordEntity> PersonalRecords { get; set; }
+    public DbSet<ProfilePhotoEntity> ProfilePhotos { get; set; }
     
     public WolferContext(DbContextOptions<WolferContext> options) : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<ProfilePhotoEntity>(entity =>
+        {
+            entity.HasKey(p => p.UserId);
+            entity.Property(p => p.ContentType).IsRequired();
+            entity.Property(p => p.Photo).IsRequired();
+            entity.HasOne<IdentityUser>(p => p.User)
+                .WithOne()
+                .HasForeignKey<ProfilePhotoEntity>(p => p.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
     }
 }
